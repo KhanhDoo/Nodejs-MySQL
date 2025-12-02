8192cdcb6d70f294fe89290e58966be4aeb051ab
KhanhDoo <d0qu0ckhanh02092003@gmail.com>
2025-12-02 12:51:10 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import configViewEngine from './config/viewEngine'
+import initWebRouters from './router/web'
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB from "./config/connectDB";
+
+dotenv.config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+configViewEngine(app);
+initWebRouters(app);
+
+connectDB();
+
+let port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log("Backend nodejs chay o cong : " + port);
+})
---
+++ src/services/CRUDService.js
@@ -0,0 +1,9 @@
+
+let createNewUser = (data) => {
+    console.log('data from service');
+    console.log(data);
+}
+
+module.exports = {
+    createNewUser: createNewUser,
+}
