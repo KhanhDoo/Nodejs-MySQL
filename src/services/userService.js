2b6573dc9942eecaca0e704bf7927e069adbf767
KhanhDoo <d0qu0ckhanh02092003@gmail.com>
2025-12-16 10:08:25 +0700

    #35 Viết APIs "Chuẩn MEN" với Node.js

---
+++ src/services/userService.js
@@ -0,0 +1,69 @@
+import db from '../models/index'
+import bcrypt, { hash } from 'bcryptjs';
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true,
+                })
+                if (user) {
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'Oke'
+                        console.log(user)
+                        delete user.password;
+                        userData.user = user;
+                    }
+                    else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password'
+                    }
+
+                }
+                else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User not found`
+                }
+
+            }
+            else {
+                userData.errCode = 1;
+                userData.errMessage = `Email khong ton tai. Hay thay doi Email!`;
+            }
+
+            resolve(userData);
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+
+let checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            })
+            if (user) {
+                resolve(true)
+            } else {
+                resolve(false)
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+module.exports = {
+    handleUserLogin: handleUserLogin,
+    checkUserEmail: checkUserEmail
+}
